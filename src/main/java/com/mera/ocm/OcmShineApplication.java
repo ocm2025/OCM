524eeff20ab20ac9e1418749f95fda32cb44004f
User <user@example.com>
2025-07-17 15:59:30 +0100

    first commit

---
+++ src/main/java/com/mera/ocm/OcmShineApplication.java
@@ -0,0 +1,13 @@
+package com.mera.ocm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcmShineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcmShineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mera/ocm/OcmShineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mera.ocm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcmShineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
